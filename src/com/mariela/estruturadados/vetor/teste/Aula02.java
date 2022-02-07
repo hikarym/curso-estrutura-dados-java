a8c6bc5589b996167c07721a1ea56d07fd42f6c7
Mariela Atausinchi Fernandez <maratausinchi@gmail.com>
2022-02-07 02:25:48 -0300

    adiciona testes da classe lista

---
+++ src/com/mariela/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,12 @@
+package com.mariela.estruturadados.vetor.teste;
+
+import com.mariela.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+	}
+
+}
