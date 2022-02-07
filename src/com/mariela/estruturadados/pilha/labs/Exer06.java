9c6a35fa4239e936083766fb9b4abe530b6a36a5
Mariela Atausinchi Fernandez <maratausinchi@gmail.com>
2022-02-07 17:12:41 -0300

    adicionar exercicio 06 de pilhas

---
+++ src/com/mariela/estruturadados/pilha/labs/Exer06.java
@@ -0,0 +1,56 @@
+package com.mariela.estruturadados.pilha.labs;
+
+import com.mariela.estruturadados.pilha.Pilha;
+
+public class Exer06 {
+
+	public static void main(String[] args) {
+
+		imprimeResultado("(-9)+(-2)+(-5)");
+
+		imprimeResultado("-11+([-5)");
+
+		imprimeResultado("{100 – [413 x (20 – 5 x 4) + 25}");
+
+		imprimeResultado("{100 – 413 x 0 + 25} : 5");
+	}
+	
+	public static void imprimeResultado(String expressao){
+		System.out.println(expressao + " está balanceado? " + 
+				verificaSimbolosBalanceados(expressao));
+	}
+	
+	final static String ABRE = "([{";
+	final static String FECHA = ")]}";
+
+	public static boolean verificaSimbolosBalanceados(String expressao){
+		
+		Pilha<Character> pilha = new Pilha<Character>();
+		int index = 0;
+		char simbolo, topo;
+		
+		while (index < expressao.length()){
+			simbolo = expressao.charAt(index);
+			
+			if (ABRE.indexOf(simbolo) > -1){
+				pilha.empilha(simbolo);
+				
+			} else if (FECHA.indexOf(simbolo) > -1){
+				
+				if (pilha.estaVazia()){
+					return false;
+				} else {
+					topo = pilha.desempilha();
+					
+					if (ABRE.indexOf(topo) != FECHA.indexOf(simbolo)){
+						return false;
+					}
+				}
+			}
+			
+			index++;
+		}
+		
+		return true;
+	}
+}
